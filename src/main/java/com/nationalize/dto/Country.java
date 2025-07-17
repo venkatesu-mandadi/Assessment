34f150b966a750bc0fea1d9b2fcf66390b5fc000
Venkatesu <golla.venkatesu@gmail.com>
2025-07-17 17:56:27 +0530

    Added countryname

--- src/main/java/com/nationalize/dto/Country.java
+++ src/main/java/com/nationalize/dto/Country.java
@@ -4,7 +4,8 @@ public class Country{
 	
     public String country_id;
     public double probability;
-   public String zipCode;
+    public String zipCode;
+    public String countryName;
     
     
 	/**
